a30115efc7de95a925d48cd8ffaf047194f17e5b
Megan Nguyen <megan.nguyenn261@gmail.com>
2020-11-15 20:38:25 -0800

    suffering

---
+++ analysis/Scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+
+# Method Chaining
+def load_and_process(path):
+    # Chain 1: Data loaded and missing data removed
+    df1 = (
+        pd.read_csv(r"..\..\data\raw\database.csv")
+            .dropna()
+            .loc[lambda x: x['Victim Sex'] != 'Unknown']
+            # Not removing victim age at 0 because i realized that could be a baby oops
+            .loc[lambda x: x['Victim Race'] != 'Unknown']
+            .loc[lambda x: x['Perpetrator Sex'] != 'Unknown']
+            .loc[lambda x: x['Perpetrator Age'] != 0]
+            .loc[lambda x: x['Perpetrator Race'] != 'Unknown']
+            .loc[lambda x: x['Relationship'] != 'Unknown']
+            .loc[lambda x: x['Weapon'] != 'Unknown']
+            .reset_index(drop=True)
+    )
+    # Chain 2: Drop irrelevant columns, not sure if we want to add new ones?
+    df2 = (
+        # Currently just dropping the following:
+        # Agency name, Agency Type, Victim & Perp Ethnicity (these columns are somehow different from race.)
+        df1.drop(['Agency Code', 'Agency Name', 'Agency Type', 'Victim Ethnicity', 'Perpetrator Ethnicity'], axis=1)
+            .reset_index(drop=True)
+    )
+    return df2
